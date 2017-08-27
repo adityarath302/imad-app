ad80e2f24b900280ac53c4b9638b20744cf7d691
adityarath302 <31166600+adityarath302@users.noreply.github.com>
2017-08-28 00:52:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,11 +47,11 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
-   var names=['name1','name2','name3'];
+   var names=['name1','name2','name3','name4'];
    var list ='';
    for(var i=0;i<name.length;i++)
    {
-       list+='<li'+names[i]+'</li>';
+       list+='<li>'+names[i]+'</li>';
        
    }//
 var ul=document.getElementById('namelist');
